a3169aa6d754be6387722b1ad25810da3e357649
airbreak <jiangjianming_m@163.com>
2018-04-08 00:09:14 +0800

    add npc

--- js/databus.js
+++ js/databus.js
@@ -1,36 +1,52 @@
-import  Pool from './base/pool'
+import Pool from './base/pool'
 
 let instance
 
-/*
-* 全局状态管理器
+/**
+ * 全局状态管理器
  */
-export default  class DataBus {
-    constructor(){
-        if( instance )
-            return instance
-        instance = this
-        this.pool = new Pool()
-        this.reset()
-    }
+export default class DataBus {
+  constructor() {
+    if ( instance )
+      return instance
 
-    reset () {
-        this.frame = 0
-        this.score = 0
-        this.bullets = []
-        this.enemys = []
-        this.animations = []
-        this.gameOver = false
-    }
+    instance = this
 
-    /*
-    * 回收子弹，进入对象池
-    * 从此不进入帧循环
-     */
-    removeBullets(bullet) {
-        let temp = this.bullets.shift()
+    this.pool = new Pool()
 
-        temp.visible = false
-        this.pool.recover('bulle',bullet)
-    }
+    this.reset()
+  }
+
+  reset() {
+    this.frame      = 0
+    this.score      = 0
+    this.bullets    = []
+    this.enemys     = []
+    this.animations = []
+    this.gameOver   = false
+  }
+
+  /**
+   * 回收敌人，进入对象池
+   * 此后不进入帧循环
+   */
+  removeEnemey(enemy) {
+    let temp = this.enemys.shift()
+
+    temp.visible = false
+
+    this.pool.recover('enemy', enemy)
+  }
+
+  /**
+   * 回收子弹，进入对象池
+   * 此后不进入帧循环
+   */
+  removeBullets(bullet) {
+    let temp = this.bullets.shift()
+
+    temp.visible = false
+
+    this.pool.recover('bullet', bullet)
+  }
 }
